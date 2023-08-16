58f5329fca860f0862b1bb79d6cc71849a1cf670
BinodMagar60 <kauchabinod88@gmail.com>
2023-08-16 20:20:37 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let hrs = document.querySelector("#hrs-an");
+let min = document.querySelector("#min-an");
+let sec = document.querySelector("#sec-an");
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * 6;
+  let ss = day.getSeconds() * 6;
+
+  hrs.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
+});
