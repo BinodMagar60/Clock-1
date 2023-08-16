9075458b6e3fa11d2788beeb8bfa0a205db97c7e
BinodMagar60 <kauchabinod88@gmail.com>
2023-08-16 20:35:38 +0545

    second commit

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ setInterval(() => {
   min.style.transform = `rotateZ(${mm}deg)`;
   sec.style.transform = `rotateZ(${ss}deg)`;
 });
+
+
+let hr = document.querySelector('#hrs-dg');
+let mn = document.querySelector('#min-dg');
+let sc = document.querySelector('#sec-dg');
+setInterval(() => {
+    let currentTime = new Date();
+    hr.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours() ;
+    mn.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes() ;
+    sc.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds() ;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
